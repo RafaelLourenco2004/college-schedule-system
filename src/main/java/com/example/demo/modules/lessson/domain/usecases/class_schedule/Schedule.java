a09ccb1eaf69fface9dc2e001f4a8226fba975c1
Rafael <rafael.lourenco816@gmail.com>
2025-01-04 21:46:58 -0300

    .

---
+++ src/main/java/com/example/demo/modules/lessson/domain/usecases/class_schedule/Schedule.java
@@ -0,0 +1,89 @@
+package com.example.demo.modules.lessson.domain.usecases.class_schedule;
+
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+import com.example.demo.modules.lessson.domain.entities.Lesson;
+import com.example.demo.modules.lessson.domain.entities.LessonDate;
+import com.example.demo.modules.lessson.domain.entities.LessonTime;
+import com.example.demo.modules.lessson.domain.entities.Weekday;
+import com.example.demo.modules.lessson.domain.exceptions.EntityAlreadyExistsException;
+import com.example.demo.modules.lessson.domain.exceptions.InvalidAttributeValueException;
+
+public class Schedule {
+
+    private Map<Weekday, Map<LocalTime, Lesson>> schedule;
+
+    public Schedule(String startClasses, String endClasses) {
+        schedule = getSchedule(LessonTime.toLessonTime(startClasses), LessonTime.toLessonTime(endClasses));
+    }
+
+    private Map<Weekday, Map<LocalTime, Lesson>> getSchedule(LessonTime startClasses, LessonTime endClasses) {
+        if (startClasses == null || endClasses == null)
+            throw new InvalidAttributeValueException("The times provided is invalid.");
+
+        Map<Weekday, Map<LocalTime, Lesson>> newSchedule = new HashMap<>();
+        Map<LocalTime, Lesson> scheduleSlot = new HashMap<>();
+
+        for (LessonTime lessonTime : LessonTime.values()) {
+            if (isWithinTimeRange(startClasses, endClasses, lessonTime))
+                scheduleSlot.put(LocalTime.parse(lessonTime.getTime()), null);
+        }
+
+        for (Weekday weekday : Weekday.values())
+            newSchedule.put(weekday, scheduleSlot);
+
+        return newSchedule;
+    }
+
+    private boolean isWithinTimeRange(LessonTime startTime, LessonTime endTime, LessonTime time) {
+        return !isEarlier(startTime, time) && !isLater(endTime, time);
+    }
+
+    private boolean isEarlier(LessonTime time1, LessonTime time2) {
+        LocalTime localTime1 = LocalTime.parse(time1.getTime());
+        LocalTime localTime2 = LocalTime.parse(time2.getTime());
+        return localTime2.isBefore(localTime1);
+    }
+
+    private boolean isLater(LessonTime time1, LessonTime time2) {
+        LocalTime localTime1 = LocalTime.parse(time1.getTime());
+        LocalTime localTime2 = LocalTime.parse(time2.getTime());
+        return localTime2.isAfter(localTime1);
+    }
+
+    public void setLesson(Lesson lesson) {
+        List<LessonDate> lessonDates = lesson.getDates();
+
+        Map<LocalTime, Lesson> scheduleSlot;
+        for (LessonDate lessonDate : lessonDates) {
+            scheduleSlot = schedule.get(lessonDate.getWeekDay());
+            if (scheduleSlot.containsKey(lessonDate.getStartTime()))
+                if (scheduleSlot.get(lessonDate.getStartTime()) == null){
+                    scheduleSlot.put(lessonDate.getStartTime(), lesson);
+                    schedule.put(lessonDate.getWeekDay(), scheduleSlot);
+                }
+                else
+                    throw new EntityAlreadyExistsException(
+                            String.format("The schedule slot %s - %s is already filled",
+                                    lessonDate.getWeekDay().getWeekDay(), lessonDate.getStartTime().toString()));
+            else {
+                throw new InvalidAttributeValueException(
+                        String.format("The lesson %s is not within the specified time range.",
+                                lesson.getId().toString()));
+            }
+        }
+    }
+
+    public List<Lesson> getLessons() {
+        List<Lesson> scheduledLessons = schedule.values().stream()
+                .flatMap((value) -> value.values().stream())
+                .filter((lesson) -> lesson != null)
+                .distinct()
+                .toList();
+        return scheduledLessons;
+    }
+}
--- src/main/java/com/example/demo/modules/lessson/domain/usecases/class_schedule/SetClassSchedule.java
+++ src/main/java/com/example/demo/modules/lessson/domain/usecases/class_schedule/SetClassSchedule.java
@@ -1,21 +1,16 @@
 package com.example.demo.modules.lessson.domain.usecases.class_schedule;
 
-import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.modules.lessson.domain.entities.Lesson;
-import com.example.demo.modules.lessson.domain.entities.LessonDate;
 import com.example.demo.modules.lessson.domain.entities.LessonId;
-import com.example.demo.modules.lessson.domain.entities.LessonTime;
 import com.example.demo.modules.lessson.domain.entities.Student;
-import com.example.demo.modules.lessson.domain.entities.Weekday;
 import com.example.demo.modules.lessson.domain.exceptions.EntityAlreadyExistsException;
 import com.example.demo.modules.lessson.domain.exceptions.InvalidAttributeValueException;
 import com.example.demo.modules.lessson.domain.usecases.lesson.LessonGetService;
@@ -26,92 +21,37 @@ public class SetClassSchedule {
     @Autowired
     private LessonGetService lessonGetService;
 
-    private Map<Weekday, Map<LocalTime, Lesson>> schedule;
+    private Schedule schedule;
 
     public List<Lesson> setClassSchedule(List<LessonId> requiredLessonsId, Student student,
             String startClasses, String endClasses) {
 
-        initializeSchedule(LessonTime.toLessonTime(startClasses), LessonTime.toLessonTime(endClasses));
+        schedule = new Schedule(startClasses, endClasses);
 
         List<Lesson> requiredLessons = requiredLessonsId.stream().map(
-                (id) -> lessonGetService.getOne(id)).toList();
-        requiredLessons.stream().forEach((lesson) -> setLesson(lesson));
+                (id) -> lessonGetService.getOne(id))
+                .collect(Collectors.toCollection(ArrayList::new));
+        requiredLessons.stream().forEach((lesson) -> schedule.setLesson(lesson));
 
-        Collections.sort(requiredLessons, (a, b) -> compareLessonPerWeeklyClasses(a, b));
+        List<Lesson> lessons = lessonGetService.getAll();
+        Collections.sort(lessons, (a, b) -> compareLessonPerWeeklyClasses(a, b));
+        lessons.forEach((lesson) -> System.out.printf("Lesson : %s", lesson.getSubject().getName()));
 
-        for (Lesson lesson : requiredLessons) {
+        for (Lesson lesson : lessons) {
             try {
-                setLesson(lesson);
+                schedule.setLesson(lesson);
             } catch (EntityAlreadyExistsException e) {
+                System.out.println("ENTITY ALREADY EXISTS");
                 continue;
             } catch (Exception e) {
                 throw new InvalidAttributeValueException("Something went wrong!");
             }
         }
-
-        List<Lesson> scheduledLessons = schedule.values().stream()
-                .flatMap((value) -> value.values().stream())
-                .filter((lesson) -> lesson != null)
-                .toList();
-
+        List<Lesson> scheduledLessons = schedule.getLessons();
         return scheduledLessons;
     }
 
     private int compareLessonPerWeeklyClasses(Lesson a, Lesson b) {
-        return b.getWeeklyClasses() - a.getWeeklyClasses();
-    }
-
-    private void initializeSchedule(LessonTime startClasses, LessonTime endClasses) {
-        if (startClasses == null || endClasses == null)
-            throw new InvalidAttributeValueException("The times provided is invalid.");
-
-        schedule = new HashMap<>();
-
-        Map<LocalTime, Lesson> scheduleSlot = new HashMap<>();
-        for (Weekday weekday : Weekday.values()) {
-            for (LessonTime lessonTime : LessonTime.values()) {
-                if (isWithinTimeRange(startClasses, endClasses, lessonTime)) {
-                    scheduleSlot.put(LocalTime.parse(lessonTime.getTime()), null);
-                    schedule.put(weekday, null);
-                }
-            }
-        }
-    }
-
-    private void setLesson(Lesson lesson) {
-        List<LessonDate> lessonDates = lesson.getDates();
-
-        Map<LocalTime, Lesson> scheduleSlot;
-        for (LessonDate lessonDate : lessonDates) {
-            scheduleSlot = schedule.get(lessonDate.getWeekDay());
-            if (scheduleSlot.containsKey(lessonDate.getStartTime()))
-                if (scheduleSlot.get(lessonDate.getStartTime()) != null)
-                    scheduleSlot.put(lessonDate.getStartTime(), lesson);
-                else
-                    throw new EntityAlreadyExistsException(
-                            String.format("The schedule slot %s - %s is already filled",
-                                    lessonDate.getWeekDay().getWeekDay(), lessonDate.getStartTime().toString()));
-            else {
-                throw new InvalidAttributeValueException(
-                        String.format("The lesson %s is not within the specified time range.",
-                                lesson.getId().toString()));
-            }
-        }
-    }
-
-    private boolean isWithinTimeRange(LessonTime startTime, LessonTime endTime, LessonTime time) {
-        return !isEarlier(startTime, time) && !isLater(endTime, time);
-    }
-
-    private boolean isEarlier(LessonTime time1, LessonTime time2) {
-        LocalTime localTime1 = LocalTime.parse(time1.getTime());
-        LocalTime localTime2 = LocalTime.parse(time2.getTime());
-        return localTime2.isBefore(localTime1);
-    }
-
-    private boolean isLater(LessonTime time1, LessonTime time2) {
-        LocalTime localTime1 = LocalTime.parse(time1.getTime());
-        LocalTime localTime2 = LocalTime.parse(time2.getTime());
-        return localTime2.isAfter(localTime1);
+        return a.getWeeklyClasses() - b.getWeeklyClasses();
     }
 }
