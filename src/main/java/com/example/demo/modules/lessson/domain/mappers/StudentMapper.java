07917130979a6a322c607a80e92493bdb7204381
Rafael <rafael.lourenco816@gmail.com>
2025-03-08 23:56:39 -0300

    .

--- src/main/java/com/example/demo/modules/lessson/domain/mappers/StudentMapper.java
+++ src/main/java/com/example/demo/modules/lessson/domain/mappers/StudentMapper.java
@@ -1,5 +1,6 @@
 package com.example.demo.modules.lessson.domain.mappers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.example.demo.modules.lessson.domain.dtos.LessonDto;
@@ -7,16 +8,20 @@ import com.example.demo.modules.lessson.domain.dtos.StudentDto;
 import com.example.demo.modules.lessson.domain.entities.Student;
 
 public class StudentMapper {
-    
-    public static StudentDto toDto(Student student){
+
+    public static StudentDto toDto(Student student) {
         StudentDto dto = new StudentDto(student.getId(), student.getName());
 
-        List<LessonDto> lessons = student.getLessons().stream()
-            .map((lesson) -> LessonMapper.toDto(lesson))
-            .toList();
+        List<LessonDto> lessons;
+        if (student.getLessons() != null) { 
+            lessons = student.getLessons().stream()
+                    .map((lesson) -> LessonMapper.toDto(lesson))
+                    .toList();
+        } else
+            lessons = new ArrayList<>();
 
         dto.setLessons(lessons);
-        
+
         return dto;
     }
 }
